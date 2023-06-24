04d419eb904c1bb953f3f5c35197156e04356fe4
heejit <jitenshah78@gmail.com>
2023-06-25 03:17:13 +0530

    HBoxRationLayout added

---
+++ layout_demo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+
+func CreateHBoxRatioutLayoutWidget() *fyne.Container {
+	lbl1 := widget.NewLabel("Name:")
+	entry1 := widget.NewEntry()
+	lbl2 := widget.NewLabel("Number:")
+	entry2 := widget.NewEntry()
+	btn  := widget.NewButton("okay", nil)
+	content := container.New(NewHBoxRatioLayout([]float32{10, 20, 10, 20, 5, 15}), lbl1, entry1, lbl2, entry2, widget.NewSeparator(), btn)
+	return content
+}
